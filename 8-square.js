6e480480efcf394fc38ddc81e4efd850ea02cebc
Qhama Hani <qhamahani@gmail.com>
2025-11-06 01:47:20 +0200

    Add a square

---
+++ 8-square.js
@@ -0,0 +1,8 @@
+let number = parseInt(process.argv[2]);
+if (isNaN(number)) {
+  console.log("Missing size");
+} else {
+  for (let i = 0; i < number; i++) {
+    console.log("X".repeat(number));
+  }
+}
