8ded6c973c5721a09714bb3805d47aa951d27ec7
Qhama Hani <qhamahani@gmail.com>
2025-11-05 19:15:34 +0200

    Add 2-arguments file and add code to it

---
+++ 2-arguments.js
@@ -0,0 +1,9 @@
+const countArgs = process.argv.length - 2;
+
+if (countArgs === 0) {
+  console.log("No argument");
+} else if (countArgs === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
