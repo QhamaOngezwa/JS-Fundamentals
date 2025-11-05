1dc4d987c4559354e9d7e1dfdbac475e8891bf4d
Qhama Hani <qhamahani@gmail.com>
2025-11-06 00:16:16 +0200

    Add while loop

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,15 +1,6 @@
-// if (isNaN(process.argv[2])) {
-//   console.log("Not a number");
-// } else {
-//   let number = parseInt(process.argv[2]);
-//   while (number > 0) {
-//     console.log("C is fun");
-//   }
-//   number--;
-// }
-let number = process.argv[2];
-if (isNaN(number) || number <= 0) {
+for (let i = 0; i < parseInt(process.argv[2]); i++) {
+  console.log("C is fun");
+}
+if (isNaN(process.argv[2]) || process.argv[2] <= 0) {
   console.log("Missing number of occurrences");
-} else {
-  console.log("C is fun\n".repeat(number).trim());
 }
