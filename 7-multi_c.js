cb881ea6fa44740e67c2e1827ce653dd465c0312
Qhama Hani <qhamahani@gmail.com>
2025-11-06 00:37:53 +0200

    Add if statement

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,6 +1,8 @@
-for (let i = 0; i < parseInt(process.argv[2]); i++) {
-  console.log("C is fun");
-}
-if (isNaN(process.argv[2])) {
+let number = parseInt(process.argv[2]);
+if (isNaN(number)) {
   console.log("Missing number of occurrences");
+} else {
+  for (let i = 0; i < number; i++) {
+    console.log("C is fun");
+  }
 }
